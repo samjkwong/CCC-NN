4c030db87ddf4f3eca2f1836f0ebb33826384e27
Sam Kwong <samkwong@stanford.edu>
2019-02-26 15:33:19 -0800

    .

--- net.py
+++ net.py
@@ -1,6 +1,5 @@
 from collections import namedtuple
 import sys
-from typing import List, Tuple, Dict, Set, Union
 import torch
 import torch.nn as nn
 import torch.nn.utils
@@ -10,15 +9,10 @@ import numpy as np
 import create_data as cd
 #import matplotlib.pyplot as plt
 
-examples = cd.create_examples()
-np.random.shuffle(examples)
-#train_examples = examples[:9*len(examples)//10]
-#test_examples = examples[9*len(examples)//10:]
-X, Y = cd.generate_tensors(examples)
 
-class Classification(nn.Module):
+class Net(nn.Module):
     def __init__(self, vocab_size, dropout=0.2):
-        super(Classification, self).__init__()
+        super(Net, self).__init__()
 
         self.linear1 = nn.Linear(in_features=vocab_size, out_features=50)
         self.relu1 = nn.ReLU()
@@ -37,35 +31,3 @@ class Classification(nn.Module):
         a3 = self.out(h2)
         y = self.out_activation(a3)
         return y
-
-net = Net()
-opt = optim.Adam(net.parameters(), lr=0.001, betas=(0.9, 0.999))
-criterion = nn.BCELoss()
-
-def train_epoch(model, opt, criterion, batch_size=50):
-    model.train()
-    losses = []
-    for beg_i in range(0, X.size(0), batch_size):
-        x_batch = X[beg_i:beg_i + batch_size, :]
-        y_batch = Y[beg_i:beg_i + batch_size, :]
-        x_batch = Variable(x_batch)
-        y_batch = Variable(y_batch)
-
-        opt.zero_grad()
-        # (1) Forward
-        y_hat = net(x_batch)
-        # (2) Compute diff
-        loss = criterion(y_hat, y_batch)
-        # (3) Compute gradients
-        loss.backward()
-        # (4) update weights
-        opt.step()        
-        losses.append(loss.data.numpy())
-    return losses
-
-e_losses = []
-num_epochs = 20
-for e in range(num_epochs):
-    e_losses += train_epoch(net, opt, criterion)
-#plt.plot(e_losses)
-print(e_losses)
